6e6bcd04a446e245f46f131ba5e5101af98ace3f
PaskalManik <paskalirvaldi10@gmail.com>
2024-04-29 15:50:15 +0700

    desain landingpage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,147 @@
 @extends ('layouts.master')
 
 @section ('tubes')
-<h1>Landing Page</h1>
+
+<div class="hero-wrap" style="background-image: url('images/img1.jpeg');">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="row no-gutters slider-text align-items-center" data-scrollax-parent="true">
+            <div class="col-md-7 ftco-animate">
+                <h1 class="mb-4">Selamat Datang di KAI Access</h1>
+                <p class="caps">Aplikasi berbasis website untuk jasa pemesanan tiket</p>
+                <a href="/login" class="btn btn-primary">Pesan Sekarang!</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<section class="ftco-section">
+    <div class="container">
+        <div class="row justify-content-center pb-4">
+            <div class="col-md-12 heading-section text-center ftco-animate">
+                <span class="subheading">Destination</span>
+                <h2 class="mb-4">Top Destination</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4 ftco-animate">
+                <div class="destination-wrap">
+                    <a class="img" style="background-image: url(images/img3.jpeg);">
+                    </a>
+                    <div class="text p-4">
+                        <h3><a>Keraton Yogyakarta</a></h3>
+                        <p class="location"><i class="fa-solid fa-location-dot"></i> Jl. Rotowijayan Blok No. 1,
+                            Panembahan, Kecamatan Kraton, Kota Yogyakarta, Daerah Istimewa Yogyakarta</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 ftco-animate">
+                <div class="destination-wrap">
+                    <a class="img" style="background-image: url(images/img3.jpeg);">
+                    </a>
+                    <div class="text p-4">
+                        <h3><a>Tangkuban Perahu</a></h3>
+                        <p class="location"><i class="fa-solid fa-location-dot"></i> Cikahuripan, Kec. Lembang, Kabupaten
+                            Bandung Barat, Jawa Barat</p>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 ftco-animate">
+                <div class="destination-wrap">
+                    <a class="img" style="background-image: url(images/img3.jpeg);">
+                    </a>
+                    <div class="text p-4">
+                        <h3><a>Monumen Nasional</a></h3>
+                        <p class="location"><i class="fa-solid fa-location-dot"></i> Gambir, Kecamatan Gambir, Kota
+                            Jakarta Pusat, Daerah Khusus Ibukota Jakarta</p>
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 ftco-animate">
+                <div class="destination-wrap">
+                    <a class="img" style="background-image: url(images/img3.jpeg);">
+                    </a>
+                    <div class="text p-4">
+                        <h3><a>Lawang Sewu</a></h3>
+                        <p class="location"><i class="fa-solid fa-location-dot"></i> Jl. Pemuda, Sekayu, Kec. Semarang
+                            Tengah, Kota Semarang, Jawa Tengah</p>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 ftco-animate">
+                <div class="destination-wrap">
+                    <a class="img" style="background-image: url(images/img3.jpeg);">
+                    </a>
+                    <div class="text p-4">
+                        <h3><a>Tanah Lot</a></h3>
+                        <p class="location"><i class="fa-solid fa-location-dot"></i> Beraban, Kec. Kediri, Kabupaten
+                            Tabanan, Bali</p>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 ftco-animate">
+                <div class="destination-wrap">
+                    <a class="img" style="background-image: url(images/img3.jpeg);">
+                    </a>
+                    <div class="text p-4">
+                        <h3><a>Museum Angkut</a></h3>
+                        <p class="location"><i class="fa-solid fa-location-dot"></i> Jl. Terusan Sultan Agung No.2,
+                            Ngaglik, Kec. Batu, Kota Batu, Jawa Timur</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<footer class="ftco-footer bg-bottom ftco-no-pt" style="background-image: url(images/img1.jpeg);">
+    <div class="container">
+        <div class="row mb-5">
+            <div class="col-md pt-5">
+                <div class="ftco-footer-widget pt-md-5 mb-4">
+                    <h2 class="ftco-heading-2">About</h2>
+                    <p>Layanan booking tiket kereta api online melalui www.tiketkai.com dan aplikasi TiketKAI Mobile. 
+                    Beli tiket kereta jadi cepat, nyaman dan mudah. TiketKAI.com dikelola oleh PT Bimasakti Multi Sinergi.</p>
+                    <ul class="ftco-footer-social list-unstyled float-md-left float-lft">
+                    <ul class="ftco-footer-social list-unstyled float-md-left float-lft">
+                        <li class="ftco-animate"><a href="#" target="_blank"><i class="fa-brands fa-facebook"></i> Kereta Api Kita</a></li>
+                        <li class="ftco-animate"><a href="#" target="_blank"><i class="fa-brands fa-twitter"> Kereta Api Indonesia</i></a></li>
+                        <li class="ftco-animate"><a href="#" target="_blank"><i class="fa-brands fa-instagram"> keretaapikita</i></a></li>
+                    </ul>
+
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md pt-5 border-left">
+                <div class="ftco-footer-widget pt-md-5 mb-4">
+                    <h2 class="ftco-heading-2">Menu</h2>
+                    <ul class="list-unstyled">
+                        <li><span>Home</span></a></li>
+                        <li><span>About</span></a></li>
+                        <li><span>Contact </span></a></li>
+                        <li><span>Destination </span></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md pt-5 border-left">
+                <div class="ftco-footer-widget pt-md-5 mb-4">
+                    <h2 class="ftco-heading-2">Have a Questions?</h2>
+                    <div class="block-23 mb-3">
+                        <ul>
+                            <li><i class="fa-solid fa-location-dot"></i><span class="text"> Jl. Prof. H. M. Yamin No.13, Gg. Buntu, Kec. Medan Tim., Kota Medan, Sumatera Utara 20236</span></li>
+                            <li><a href="#"><i class="fa-brands fa-whatsapp"></i></i><span class="text"> 0121021121</span></a></li>
+                            <li><a href="#"><i class="fa-solid fa-envelope"></i></i></i><span class="text"> cs@kai.id</span></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
 @endsection
