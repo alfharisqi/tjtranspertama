a054819458e46d27f96312665ceec8724eff6f8c
Mayadi21 <mayadisilalahi@gmail.com>
2024-05-12 23:17:24 +0700

    gender fixed & dummy

--- database/seeders/DummyUsersSeeder.php
+++ database/seeders/DummyUsersSeeder.php
@@ -8,24 +8,18 @@ use Illuminate\Database\Seeder;
 
 class DummyUsersSeeder extends Seeder
 {
-    /**
+    /** 
      * Run the database seeds.
      */
     public function run(): void
     {
         $userData = [
-            [
-                'name'=>'Mas user',
-                'email'=>'user@gmail.com',
-                'role'=>'user',
-                'password'=>bcrypt('123456')
-            ],
-            [
+            [   'id' => 1,
                 'name'=>'Mas admin',
-                'email'=>'admin@gmail.com',
+                'email'=>'pppkelompok5@gmail.com',
                 'role'=>'admin',
-                'password'=>bcrypt('123456')
-            ],
+                'password'=>bcrypt('admin12345')
+            ]
             ];
 
             foreach($userData as $key => $val){
