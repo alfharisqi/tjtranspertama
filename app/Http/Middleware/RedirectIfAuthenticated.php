e97a72d7d3b1e60b6e7477d6985ca23d126f022d
brisbane26 <brisbanejrsihombing@gmail.com>
2024-05-13 20:40:01 +0700

    verifikasi email

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -2,28 +2,27 @@
 
 namespace App\Http\Middleware;
 
-use App\Providers\RouteServiceProvider;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Redirect;
 
 class RedirectIfAuthenticated
 {
-    public function handle(Request $request, Closure $next, string ...$guards)
+    public function handle(Request $request, Closure $next, ...$guards)
     {
         $guards = empty($guards) ? [null] : $guards;
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                return redirect()->intended('/'); // Arahkan ke halaman home jika sudah terotentikasi
             }
         }
 
         // Jika pengguna belum terautentikasi dan mencoba mengakses halaman login atau register
-        // maka arahkan mereka ke halaman landing
+        // maka biarkan mereka mengaksesnya
         if ($request->is('login') || $request->is('register')) {
-            return redirect('/');
+            return $next($request);
         }
 
         // Tambahkan pengecualian untuk rute /checkprice
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use App\Models\User; 
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -22,7 +24,13 @@ class AuthServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->registerPolicies();
-
-        //
+    
+        Gate::define('isAdmin', function (User $user) {
+            return $user->isAdmin(); // Misalnya, method isAdmin() ada di model User
+        });
+    
+        Gate::define('isCustomer', function (User $user) {
+            return $user->isCustomer(); // Misalnya, method isCustomer() ada di model User
+        });
     }
 }
