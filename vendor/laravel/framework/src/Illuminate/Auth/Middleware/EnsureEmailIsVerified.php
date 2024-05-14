a50a7ca70dcf33ac55fc7c27b4e04d53827d3bc3
Mayadi21 <mayadisilalahi@gmail.com>
2024-05-14 21:33:46 +0700

    kenapa sih harus commit?

--- vendor/laravel/framework/src/Illuminate/Auth/Middleware/EnsureEmailIsVerified.php
+++ vendor/laravel/framework/src/Illuminate/Auth/Middleware/EnsureEmailIsVerified.php
@@ -4,12 +4,22 @@ namespace Illuminate\Auth\Middleware;
 
 use Closure;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\URL;
 
 class EnsureEmailIsVerified
 {
+    /**
+     * Specify the redirect route for the middleware.
+     *
+     * @param  string  $route
+     * @return string
+     */
+    public static function redirectTo($route)
+    {
+        return static::class.':'.$route;
+    }
+
     /**
      * Handle an incoming request.
      *
@@ -20,20 +30,12 @@ class EnsureEmailIsVerified
      */
     public function handle($request, Closure $next, $redirectToRoute = null)
     {
-        // Memeriksa apakah pengguna telah diotentikasi
-        if (! $request->user()) {
-            return $next($request);
-        }
-
-        // Memeriksa apakah pengguna merupakan instance dari MustVerifyEmail dan belum memverifikasi email
-        if ($request->user() instanceof MustVerifyEmail && ! $request->user()->hasVerifiedEmail()) {
-            // Memeriksa apakah permintaan adalah JSON
-            if ($request->expectsJson()) {
-                return abort(403, 'Your email address is not verified.');
-            }
-
-            // Mengarahkan pengguna ke halaman verifikasi email
-            return Redirect::guest(URL::route($redirectToRoute ?: 'verification.notice'));
+        if (! $request->user() ||
+            ($request->user() instanceof MustVerifyEmail &&
+            ! $request->user()->hasVerifiedEmail())) {
+            return $request->expectsJson()
+                    ? abort(403, 'Your email address is not verified.')
+                    : Redirect::guest(URL::route($redirectToRoute ?: 'verification.notice'));
         }
 
         return $next($request);
