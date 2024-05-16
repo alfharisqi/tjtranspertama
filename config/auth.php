aa308396c98e8a68c556ee1520caeefef0df00ef
brisbane26 <brisbanejrsihombing@gmail.com>
2024-05-16 17:05:15 +0700

    forgot-password

--- config/auth.php
+++ config/auth.php
@@ -93,7 +93,7 @@ return [
     'passwords' => [
         'users' => [
             'provider' => 'users',
-            'table' => 'password_reset_tokens',
+            'table' => 'password_resets',
             'expire' => 180,
             'throttle' => 60,
         ],
