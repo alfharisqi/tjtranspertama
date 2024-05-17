177db31b01fac3bae64036aa56efd85c3335b0f7
petrr4 <petraik111@gmail.com>
2024-05-17 22:43:19 +0700

    p

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Sonic</title>
-    <link rel="icon" type="image/png" href="{{ asset('favicon22.png') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
