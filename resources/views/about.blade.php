93a27c70b4d089c92010fedab94fdf2d91918371
syuja aql <syuja.aqilsky@gmail.com>
2024-05-23 18:17:07 +0700

    view auth & abaut

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.main')
 
 @section('front-end')
+<link rel="stylesheet" href="{{ asset('plugins/css-components-main/cards/card-8/style.css') }}">
+
 <x-front-navbar></x-front-navbar>
 
 <section class="hero-wrap hero-wrap-2 js-fullheight" style="background-image: url('images/train2.jpg'); width: 100%">
@@ -50,77 +52,230 @@
 				<h2 class="mb-4">Our Team</h2>
 			</div>
 		</div>
-		<div class="row ftco-animate">
-			<div class="col-md-12">
-				<div class="item">
-					<div class="testimony-wrap py-4">
-						<div class="text">
-							<div class="d-flex align-items-center">
-								<div class="user-img" style="background-image: url(images/bane2.jpg)"></div>
-								<div class="pl-3">
-									<p class="name">Brisbane Sihombing</p>
-									<span class="position">Back End Developer</span>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="item">
-					<div class="testimony-wrap py-4">
-						<div class="text">
-							<div class="d-flex align-items-center">
-								<div class="user-img" style="background-image: url(images/mayadi.jpg)"></div>
-								<div class="pl-3">
-									<p class="name">Mayadi Silalahi</p>
-									<span class="position">Back End Developerk</span>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="item">
-					<div class="testimony-wrap py-4">
-						<div class="text">
-							<div class="d-flex align-items-center">
-								<div class="user-img" style="background-image: url(images/paskal.jpg)"></div>
-								<div class="pl-3">
-									<p class="name">Paskal Manik</p>
-									<span class="position">Back End Developer</span>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="item">
-					<div class="testimony-wrap py-4">
-						<div class="text">
-							<div class="d-flex align-items-center">
-								<div class="user-img" style="background-image: url(images/petra.jpg)"></div>
-								<div class="pl-3">
-									<p class="name">Petra Keliat</p>
-									<span class="position">Front End Developer</span>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
+		{{-- <div class="row ftco-animate"> --}}
 
-				<div class="item">
-					<div class="testimony-wrap py-4">
-						<div class="text">
-							<div class="d-flex align-items-center">
-								<div class="user-img" style="background-image: url(images/jak.jpg)"></div>
-								<div class="pl-3">
-									<p class="name">Syuja Aqilsyah</p>
-									<span class="position">Front End Developer</span>
+			{{-- <style>
+				* {
+					font-family: Nunito, sans-serif;
+				  }
+				  
+				  .text-blk {
+					margin-top: 0px;
+					margin-right: 0px;
+					margin-bottom: 0px;
+					margin-left: 0px;
+					line-height: 25px;
+				  }
+				  
+				  .responsive-cell-block {
+					min-height: 75px;
+				  }
+				  
+				  .responsive-container-block {
+					min-height: 75px;
+					height: fit-content;
+					width: 100%;
+					display: flex;
+					flex-wrap: nowrap;
+					margin-top: 0px;
+					margin-right: auto;
+					margin-bottom: 0px;
+					margin-left: auto;
+					justify-content: center;
+					overflow-x: auto;
+				  }
+				  
+				  .outer-container {
+					padding-top: 10px;
+					padding-right: 50px;
+					padding-bottom: 10px;
+					padding-left: 50px;
+					background-color: rgb(244, 252, 255);
+				  }
+				  
+				  .inner-container {
+					max-width: 1320px;
+					flex-direction: column;
+					align-items: center;
+					margin-top: 50px;
+					margin-right: auto;
+					margin-bottom: 50px;
+					margin-left: auto;
+				  }
+				  
+				  .section-head-text {
+					margin-top: 0px;
+					margin-right: 0px;
+					margin-bottom: 5px;
+					margin-left: 0px;
+					font-size: 35px;
+					font-weight: 700;
+					line-height: 48px;
+					color: rgb(0, 135, 177);
+					margin: 0 0 10px 0;
+				  }
+				  
+				  .section-subhead-text {
+					font-size: 25px;
+					color: rgb(153, 153, 153);
+					line-height: 35px;
+					max-width: 470px;
+					text-align: center;
+					margin-top: 0px;
+					margin-right: 0px;
+					margin-bottom: 60px;
+					margin-left: 0px;
+				  }
+				  
+				  .img-wrapper {
+					width: 100%;
+				  }
+				  
+				  .team-card {
+					display: flex;
+					flex-direction: column;
+					align-items: center;
+				  }
+				  
+				  .social-media-links {
+					width: 125px;
+					display: flex;
+					justify-content: space-between;
+				  }
+				  
+				  .name {
+					font-size: 25px;
+					font-weight: 700;
+					color: rgb(102, 102, 102);
+					margin-top: 0px;
+					margin-right: 0px;
+					margin-bottom: 5px;
+					margin-left: 0px;
+				  }
+				  
+				  .position {
+					font-size: 25px;
+					font-weight: 700;
+					color: rgb(0, 135, 177);
+					margin-top: 0px;
+					margin-right: 0px;
+					margin-bottom: 50px;
+					margin-left: 0px;
+				  }
+				  
+				  .team-img {
+					width: 100%;
+					height: 100%;
+					filter: grayscale(70%);
+					transition: filter 0.3s, transform 0.3s;
+				  }
+				  
+				  .team-img:hover {
+					filter: grayscale(0%);
+					transform: scale(1.1);
+				  }
+				  
+				  .team-card-container {
+					width: 18%;
+					margin: 0 1%;
+					box-sizing: border-box;
+				  }
+				  
+				  @media (max-width: 500px) {
+					.outer-container {
+					  padding: 10px 20px 10px 20px;
+					}
+				  
+					.section-head-text {
+					  text-align: center;
+					}
+					.team-card-container {
+					  width: 100%;
+					}
+					.responsive-container-block {
+					  flex-wrap: wrap;
+					}
+				  }
+				  
+				  html, body {
+					overflow-x: hidden;
+				  }
+			</style> --}}
+				
+
+			<div class="responsive-container-block outer-container">
+				<div class="responsive-container-block inner-container">
+					<div class="responsive-container-block">
+						<div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 team-card-container">
+							<div class="team-card">
+								<div class="img-wrapper">
+									<img class="team-img" src="{{ asset('images/bane2.jpg') }}">
 								</div>
+								<p class="text-blk name">
+									Brisbane Sihombing
+								</p>
+								<p class="text-blk position">
+									Back End Developer
+								</p>
+							</div>
+						</div>
+						<div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 team-card-container">
+							<div class="team-card">
+								<div class="img-wrapper">
+									<img class="team-img" src="{{ asset('images/mayadi.jpg') }}">
+								</div>
+								<p class="text-blk name">
+									Mayadi Silalahi
+								</p>
+								<p class="text-blk position">
+									Back End Developer
+								</p>
+							</div>
+						</div>
+						<div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 team-card-container">
+							<div class="team-card">
+								<div class="img-wrapper">
+									<img class="team-img" src="{{ asset('images/paskal.jpg') }}">
+								</div>
+								<p class="text-blk name">
+									Paskal Manik
+								</p>
+								<p class="text-blk position">
+									Back End Developer
+								</p>
+							</div>
+						</div>
+						<div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 team-card-container">
+							<div class="team-card">
+								<div class="img-wrapper">
+									<img class="team-img" src="{{ asset('images/petra.jpg') }}">
+								</div>
+								<p class="text-blk name">
+									Petra Keliat
+								</p>
+								<p class="text-blk position">
+									Front End Developer
+								</p>
+							</div>
+						</div>
+						<div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 team-card-container">
+							<div class="team-card">
+								<div class="img-wrapper">
+									<img class="team-img" src="{{ asset('images/blckjak.jpg') }}">
+								</div>
+								<p class="text-blk name">
+									Syuja Aqilsyah
+								</p>
+								<p class="text-blk position">
+									Front End Developer
+								</p>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	</div>
+		
+		{{-- </div> --}}
 	</div>
 </section>
 
